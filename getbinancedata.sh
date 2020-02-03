aa61aff2be4a486952128b9f3dc353714e72d0c2
Milan Sismanovic <milan@sismanovic.com>
2020-02-03 07:59:54 +0100

    multiple currencies

--- getbinancedata.sh
+++ getbinancedata.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-pair=BTCUSDT
-#datafolder=/home/tensor/binance/data/
-datafolder=/host_shared/binance/data/
+pairfile=/home/tensor/binance/pairs.txt
+#datafolder=/home/tensor/binance/data
+#datafolder=/host_shared/binance/data
+datafolder=/home/tensor/binance/data
 
-for n in  {1..60}
-do
-        timestamp=$(date "+%Y.%m.%d-%H.%M.%S.%N")
-	depthfilename=$datafolder'/'depth$pair$timestamp
-        echo retrieving $timestamp $pair 
-        #get the order book/depth of the pair
-	curl -H "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/depth?symbol='$pair >$depthfilename
+for n in {1..60}; do
+  while read pair; do
+    timestamp=$(date "+%Y.%m.%d-%H.%M.%S.%N")
+    depthfilename=$datafolder'/'depth$pair$timestamp
+    echo retrieving $timestamp $pair
+    #get the order book/depth of the pair
+    curl -sH "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/depth?symbol='$pair >$depthfilename &
 
-	depthfilename=$datafolder'/'aggtrades$pair$timestamp
-        #get the last 500 trades aggregated
-        curl -H "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair >$aggtrades
+    aggtradesfilename=$datafolder'/'aggtrades$pair$timestamp
+    #get the last 500 trades aggregated
+    curl -sH "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair >$aggtradesfilename &
 
-        #get the last 500 trades
-        #curl -H "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair
-        sleep 1
+    #get the last 500 trades
+    #curl -H "X-MBX-APIKEY: frkoQw9fUbbdk3PH2ZzxmkVA7v97AvLcDojMgQGRX9UqWQe3bwzG38U91lE7nXyr" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair
+  done <$pairfile
+  sleep 1
 done
-
