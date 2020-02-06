019010cd79a429cc44afd79fa365255ba08047aa
Milan Sismanovic <milan@sismanovic.com>
2020-02-06 08:01:16 +0100

    fixed config script path

--- getbinancedata.sh
+++ getbinancedata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source ./getbinancedataconfig.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/config
 
 for n in {1..60}; do
   for pair in $pairs; do
