6447525b2799322ea3eaaae048196863af480fb8
Milan Sismanovic <milan@sismanovic.com>
2020-02-06 06:14:49 +0100

    added .json file appendix

--- getbinancedata.sh
+++ getbinancedata.sh
@@ -5,12 +5,12 @@ source ./getbinancedataconfig.sh
 for n in {1..60}; do
   for pair in $pairs; do
     timestamp=$(date "+%Y.%m.%d-%H.%M.%S.%N")
-    depthfilename=$datafolder'/'depth$pair$timestamp
+    depthfilename=$datafolder'/'depth$pair$timestamp'.json'
     echo retrieving $timestamp $pair
     #get the order book/depth of the pair
     curl -sH "X-MBX-APIKEY: $binanceapikey" -X GET 'https://api.binance.com/api/v3/depth?symbol='$pair >$depthfilename &
 
-    aggtradesfilename=$datafolder'/'aggtrades$pair$timestamp
+    aggtradesfilename=$datafolder'/'aggtrades$pair$timestamp'.json'
     #get the last 500 trades aggregated
     curl -sH "X-MBX-APIKEY: $binanceapikey" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair >$aggtradesfilename &
 
