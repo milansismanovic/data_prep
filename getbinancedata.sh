188eb7516338c5e8a34e8b66efc2a64f4ec396d4
Milan Sismanovic <milan@sismanovic.com>
2020-02-13 07:40:23 +0100

    added a date folder for each day for the download script

--- getbinancedata.sh
+++ getbinancedata.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $DIR/config
-
+date_name=$(date '+%Y.%m.%d')
+echo putting data to $datafolder'/'$date_name
+mkdir -p $datafolder'/'$date_name
 for n in {1..60}; do
   for pair in $pairs; do
     timestamp=$(date "+%Y.%m.%d-%H.%M.%S.%N")
-    depthfilename=$datafolder'/'depth$pair$timestamp'.json'
+    depthfilename=$datafolder'/'$date_name'/'depth$pair$timestamp'.json'
     echo retrieving $timestamp $pair
     #get the order book/depth of the pair
     curl -sH "X-MBX-APIKEY: $binanceapikey" -X GET 'https://api.binance.com/api/v3/depth?symbol='$pair >$depthfilename &
 
-    aggtradesfilename=$datafolder'/'aggtrades$pair$timestamp'.json'
+    aggtradesfilename=$datafolder'/'$date_name'/'aggtrades$pair$timestamp'.json'
     #get the last 500 trades aggregated
     curl -sH "X-MBX-APIKEY: $binanceapikey" -X GET 'https://api.binance.com/api/v3/aggTrades?symbol='$pair >$aggtradesfilename &
 
