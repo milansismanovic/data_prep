f247c2106ba20339d21f58ead54932e06f8af464
Milan Sismanovic <milan@sismanovic.com>
2020-02-19 06:50:30 +0100

    use config for input and output folder

--- python/normalize.py
+++ python/normalize.py
@@ -1,9 +1,24 @@
 import json
 import numpy as np
 import os
+from envbash import load_envbash
 
-input_file_dir = '/home/tensor/binance/devdata/'
-output_file_dir = '/home/tensor/binance/devnormalizedata/'
+# run the config variables set script
+CONFIG_FILENAME = '../config'
+if not os.path.isfile('%s' % CONFIG_FILENAME):
+    print('config file named \'%s\' not found' % CONFIG_FILENAME)
+    exit(10)
+load_envbash(CONFIG_FILENAME)
+
+input_file_dir = os.environ['datafolder']
+if not os.path.exists(input_file_dir):
+    print('input_file_dir does not exist or is not configured: \'%s\'' % input_file_dir)
+    exit(10)
+
+output_file_dir = os.environ['output_file_dir']
+if not os.path.exists(output_file_dir):
+    print('output_file_dir does not exist or is not configured: \'%s\'' % output_file_dir)
+    exit(10)
 
 # target_futures_seconds = 60  # how many seconds in the future of the depth should the target be
 # target_amount_of_trades = 100
@@ -22,7 +37,7 @@ for i in range(0, len(trades_files) - 1):
     timestamp = ''
     bids = ''
     asks = ''
-    depthfilename = input_file_dir + depth_files[i]
+    depthfilename = input_file_dir + os.path.sep + depth_files[i]
     with open(depthfilename) as json_file:
         depth = json.load(json_file)
         json_file.close()
@@ -30,7 +45,7 @@ for i in range(0, len(trades_files) - 1):
     bids = np.array(depth['bids']).astype(np.float)
     asks = np.array(depth['asks']).astype(np.float)
 
-    tradesfilename = input_file_dir + trades_files[i]
+    tradesfilename = input_file_dir + os.path.sep + trades_files[i]
     with open(tradesfilename) as json_file1:
         trades_raw = json.load(json_file1)
         json_file1.close()
@@ -50,12 +65,14 @@ for i in range(0, len(trades_files) - 1):
     # price_array = np.array([pick_price(p) for p in trades_raw]).astype(np.float).astype(np.float)
     # price_avg = np.average(price_array)
     # output_data_entry = np.append(output_data_entry, price_avg)  # target
-    print(output_data_entry)
+    # print(output_data_entry)
 
     output_data.append(output_data_entry)
 
 # write fused data to csv file
-np.savetxt(output_file_dir+'fused.csv', output_data, delimiter=',')
+output_file_name = output_file_dir + os.path.sep + 'fused.csv'
+print('saving file to %s' % output_file_name)
+np.savetxt(output_file_name, output_data, delimiter=',')
 
 # enrich step
 # TODO add prices for 5,10,..,60minutes,4h,12h,24h,7d,30d,1y - do this in a normalize step
